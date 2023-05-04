7ecc42f136c8dde43d9a1499db01566e1e45d67a
wedersonf <wedymf@gmail.com>
2023-05-04 04:38:23 -0300

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .where('amount', 500)
+    .select('*')
 
-  return tables
+  return transactions
 })
 
 app
